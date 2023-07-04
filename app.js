6d0bb974cfa20208307b5569f386af01ec9b1c56
Elmar <66626113+JannaDarth@users.noreply.github.com>
2023-07-04 20:48:26 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,66 @@
+const typingZone = document.querySelector("textarea");
+const answerZone = document.querySelector(".morsed");
+const outcome = document.querySelector(".outcome");
+const morseAlphabet = {
+  A: ".-",
+  B: "-...",
+  C: "-.-.",
+  D: "-..",
+  E: ".",
+  F: "..-.",
+  G: "--.",
+  H: "....",
+  I: "..",
+  J: ".---",
+  K: "-.-",
+  L: ".-..",
+  M: "--",
+  N: "-.",
+  O: "---",
+  P: ".--.",
+  Q: "--.-",
+  R: ".-.",
+  S: "...",
+  T: "-",
+  U: "..-",
+  V: "...-",
+  W: ".--",
+  X: "-..-",
+  Y: "-.--",
+  Z: "--..",
+  " ": " / ",
+  ".": ".-.-.-",
+  "?": "..--..",
+  "!": "-.-.--",
+  ",": "--..--",
+  ":": "---...,",
+};
+const keys = Object.keys(morseAlphabet);
+
+typingZone.addEventListener("keydown", (e) => {
+  if (!keys.includes(e.key.toUpperCase()) && e.key != "Backspace") {
+    e.preventDefault();
+    console.log(`This convertor doesn't accept : ${e.key} symbol`);
+  }
+});
+
+typingZone.addEventListener("input", (e) => {
+  const input = typingZone.value.toUpperCase();
+  outcome.style.display = input.length ? "inherit" : "none";
+  if (e.inputType == "deleteContentBackward") {
+    deleteText(input);
+    return;
+  }
+  convertToMorse(e.data.toUpperCase());
+});
+
+const convertToMorse = (key) => {
+  answerZone.textContent += `${morseAlphabet[key]} `;
+};
+
+const deleteText = (input) => {
+  answerZone.textContent = "";
+  for (let i = 0; i < input.length; i++) {
+    answerZone.textContent += `${morseAlphabet[input[i]]} `;
+  }
+};
